d46fb2542135a967afe73d3a959b0f1c2717e734
Ryan Wire <ryanwire@Ryans-MacBook-Pro.local>
2019-03-13 21:03:25 +0300

    [Chore #164602075] Configures allowed hosts to include heroku app in settings.py file

--- patienttracker/patienttracker/settings.py
+++ patienttracker/patienttracker/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['serene-brook-70330.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
