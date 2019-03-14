4bcffa01ef2a23137b425f9adbbab3930e951fc6
Ryan Wire <ryanwire@Ryans-MacBook-Pro.local>
2019-03-14 18:10:23 +0300

    [Chore #164602075] Configures whitenoise for serving static files in production

--- patienttracker/patienttracker/settings.py
+++ patienttracker/patienttracker/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'patienttracker.urls'
@@ -127,3 +128,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
