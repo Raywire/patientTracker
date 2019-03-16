65dddb4327c23395137b6802a319dad17d10ba08
Ryan Wire <ryanwire@Ryans-MacBook-Pro.local>
2019-03-16 14:27:58 +0300

    [#164678436] Makes the application load the AppConfig subclass by default

--- patienttracker/patients/urls.py
+++ patienttracker/patients/urls.py
@@ -1,4 +1,3 @@
-from rest_framework.routers import DefaultRouter
 from patients.views import PatientViewSet, AppointmentViewSet
 from django.urls import path, include
 from rest_framework_extensions.routers import ExtendedSimpleRouter
