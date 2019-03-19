bdcbdad42b3cfe79696dc95a0cfb425475d637c5
Ryan Wire <ryanwire@Ryans-MacBook-Pro.local>
2019-03-19 18:32:27 +0300

    [#164734446] Adds django cors configuration

--- patienttracker/patients/urls.py
+++ patienttracker/patients/urls.py
@@ -5,6 +5,7 @@ from rest_framework_extensions.routers import ExtendedSimpleRouter
 router = ExtendedSimpleRouter()
 
 patients_router = router.register('api/v1/patients', PatientViewSet)
+appointment_router = router.register('api/v1/appointments', AppointmentViewSet)
 patients_router.register(
     'appointments', AppointmentViewSet,
     base_name='patient-appointments',
